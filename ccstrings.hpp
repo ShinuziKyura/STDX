6c828c3c115a50df6d533f48da5535a5bb94edb0
ShinuziKyura <rj.santos.pereira@gmail.com>
2018-09-25 23:06:38 +0100

    New stuff

---
+++ ccstrings.hpp
@@ -0,0 +1,9 @@
+#ifndef CCSTRINGS_HPP
+#define CCSTRINGS_HPP
+
+#include "language.hpp"
+
+// See https://akrzemi1.wordpress.com/2011/05/11/parsing-strings-at-compile-time-part-i/
+// See https://stackoverflow.com/a/15912824/7116484
+
+#endif
